277753f2032d075d71fee1ce27d1e07eb10c345d
RichardYunlong <hewei200685@hotmail.com>
2018-12-19 23:46:16 +0800

    添加DLL实例

---
+++ CDTest/CDAutoTest/GPathDll.cpp
@@ -0,0 +1,10 @@
+#include<stdexcept>
+#include"GPathDll.h"
+
+namespace MyMath 
+{	
+	double MyMath::Math::Add(double a, double b) 
+	{
+		return a + b;
+	}
+}
---
+++ CDTest/CDAutoTest/GPathDll.h
@@ -0,0 +1,9 @@
+#pragma once
+namespace MyMath 
+{
+	class Math 
+	{
+	public:
+		static _declspec(dllexport) double Add(double a, double b);
+	};
+}
